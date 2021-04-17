b4260bfe438446c8c3101306b2d31a8e5d4e11b7
BuildTools <unconfigured@null.spigotmc.org>
2021-04-17 11:04:46 +0100

    Root route

--- public/routes/index.js
+++ public/routes/index.js
@@ -4,8 +4,13 @@ const router = require("./auth");
 const authRoutes = require("./auth");
 const userRoutes = require('./user')
 const gigRoutes = require('./gig')
-const roleRoutes = require('./role')
+const roleRoutes = require('./role');
 
+const utils = require("../utils");
+
+router.get("/", (req, res) => {
+    utils.sendResponse(res, 200, "Welcome to SSU API")
+})
 
 routes.use("/", authRoutes);
 router.use("/user", userRoutes)
